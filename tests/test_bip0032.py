2488d22242674cf184792431ad98d79f52d07b14
Alexey Karyabkin <coffein.h@gmail.com>
2018-06-06 15:59:51 +0400

    added test for create-child_key and serialize_key, updated other tests

--- tests/test_bip0032.py
+++ tests/test_bip0032.py
@@ -13,19 +13,47 @@ def test_create_master_key(mnemonic_256):
     seed = create_seed(passphrase, 'P@ssw0rd')
     assert seed is not None
     assert len(seed) == 64
-    
     master_key = create_master_key_hdwallet(seed)
     assert master_key is not None
     assert type(master_key) is dict
+    assert master_key.get('version') is not None
+    assert master_key.get('key') is not None
+    assert master_key.get('depth') is not None
+    assert master_key.get('child') is not None
+    assert master_key.get('finger_print') is not None
+    assert master_key.get('chain_code') is not None
+    assert master_key.get('is_private') is not None
     assert master_key['is_private']
 
 
 def test_create_public_key(master_key_hdwallet):
     public_key = create_public_key_hdwallet(master_key_hdwallet['key'])
     assert public_key is not None
+    assert len(public_key) == 33
 
 
-def test_validate_keys(fail_key1, fail_key2, good_key):
-    assert not validate_keys(fail_key1)
-    assert not validate_keys(fail_key2)
-    assert validate_keys(good_key)
+def test_validate_private_key(fail_key1, fail_key2, good_key):
+    assert not validate_private_key(fail_key1)
+    assert not validate_private_key(fail_key2)
+    assert validate_private_key(good_key)
+
+
+def test_create_child_key(master_key_hdwallet):
+    child_key = create_child_key_hdwallet(master_key_hdwallet, 0)
+    assert child_key is not None
+    assert type(child_key) is dict
+    assert child_key.get('version') is not None
+    assert child_key.get('key') is not None
+    assert child_key.get('depth') is not None
+    assert child_key.get('child') is not None
+    assert child_key.get('finger_print') is not None
+    assert child_key.get('chain_code') is not None
+    assert child_key.get('is_private') is not None
+    assert child_key.get('is_private')
+
+
+def test_serialize_key(master_key_hdwallet):
+    serialize_key = serialize_key_hdwallet(master_key_hdwallet)
+    assert serialize_key is not None
+    assert type(serialize_key) is bytes
+    assert len(serialize_key) == 82
