e3be178c413181d5808332d6afec2650573f6b0d
4tochka <admin@bitaps.com>
2019-05-03 14:11:33 +0400

    connector

--- pybtc/connector.py
+++ pybtc/connector.py
@@ -788,8 +788,9 @@ class UTXO():
                         await conn.execute("DELETE FROM connector_utxo WHERE "
                                            "outpoint = ANY($1);", r)
                         await conn.copy_records_to_table('connector_utxo',  columns=["outpoint", "data"], records=rs)
-                        await conn.execute("UPDATE connector_utxo_state SET value = $1 "
-                                           "WHERE name = 'last_block';", lb)
+                        if lb:
+                            await conn.execute("UPDATE connector_utxo_state SET value = $1 "
+                                               "WHERE name = 'last_block';", lb)
                         await conn.execute("UPDATE connector_utxo_state SET value = $1 "
                                            "WHERE name = 'last_cached_block';", block_height)
                 self.saved_utxo += len(rs)
