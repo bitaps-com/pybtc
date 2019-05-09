74d07be268fb7adb337e38dc6b0dfdd8e4fd5266
4tochka <admin@bitaps.com>
2019-05-09 17:54:29 +0400

    connector

--- pybtc/connector/connector.py
+++ pybtc/connector/connector.py
@@ -593,7 +593,7 @@ class Connector:
                         stxo, missed = set(), set()
                         for i in tx["vIn"]:
                             inp = tx["vIn"][i]
-                            self.log.critical(">>> " + str(inp["_c_"]))
+                            self.log.critical(">>> " + str(inp))
                             outpoint = b"".join((inp["txId"], int_to_bytes(inp["vOut"])))
                             try:
                                 stxo.add(outpoint, tx["vIn"][i]["_c_"])
