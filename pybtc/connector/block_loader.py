385acb4f498778842541728a20ae2a77e46f50b6
4tochka <admin@bitaps.com>
2019-06-03 22:47:33 +0400

    connector

--- pybtc/connector/block_loader.py
+++ pybtc/connector/block_loader.py
@@ -73,10 +73,10 @@ class BlockLoader:
                     if self.last_batch_size < 1000000 and self.rpc_batch_limit < 450:
                         self.rpc_batch_limit += 50
                         self.log.warning("rpc batch limit %s " % self.rpc_batch_limit)
-                    elif self.last_batch_size >  40000000 and self.rpc_batch_limit > 80:
+                    elif self.last_batch_size >  40000000 and self.rpc_batch_limit > 100:
                         self.rpc_batch_limit -= 40
                         self.log.warning("rpc batch limit %s " % self.rpc_batch_limit)
-                    elif self.last_batch_size >  80000000 and self.rpc_batch_limit < 80:
+                    elif self.last_batch_size >  80000000 and self.rpc_batch_limit < 100:
                         self.rpc_batch_limit = 20
 
                         self.log.warning("rpc batch limit %s " % self.rpc_batch_limit)
