4f32d8296ae1567bf475ecff08b6793d3f68076d
4tochka <admin@bitaps.com>
2019-06-05 18:18:19 +0400

    connector

--- pybtc/connector/block_loader.py
+++ pybtc/connector/block_loader.py
@@ -96,7 +96,7 @@ class BlockLoader:
                     if self.last_batch_size < 200000000 and self.rpc_batch_limit < 1500:
                         self.rpc_batch_limit += 30
                         self.log.warning("rpc batch limit %s " % self.rpc_batch_limit)
-                    elif self.last_batch_size >  200000000:
+                    elif self.last_batch_size >  200000000 and self.rpc_batch_limit > 50:
                         self.rpc_batch_limit -= 30
                         self.log.warning("rpc batch limit %s " % self.rpc_batch_limit)
                 except asyncio.CancelledError:
