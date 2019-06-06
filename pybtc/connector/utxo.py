6e678ab633564ee683615829ec980ec5809a1630
4tochka <admin@bitaps.com>
2019-06-06 15:43:01 +0400

    connector

--- pybtc/connector/utxo.py
+++ pybtc/connector/utxo.py
@@ -63,8 +63,10 @@ class UTXO():
         # save to db tail from cache
         self.log.critical("app_last_block %s" % app_last_block)
         if self.checkpoints:
-            self.log.critical("create utxo checkpoint %s first %s last % " %
-                              (len(self.checkpoints), self.checkpoints[0], self.checkpoints[-1]))
+            self.log.critical("create utxo checkpoint %s first %s last %s " %
+                              (len(self.checkpoints),
+                               self.checkpoints[0],
+                               self.checkpoints[-1]))
         if  self.save_process or not self.cached:
             self.log.critical("create utxo checkpoint canceled %s" % str((self.save_process,
                                                                len( self.cached))))
