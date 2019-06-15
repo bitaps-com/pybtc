8c226551e3c119c0c56e59bd82e3c871106af7c7
4tochka <admin@bitaps.com>
2019-06-15 18:37:14 +0400

    merkle tree functions

--- pybtc/connector/connector.py
+++ pybtc/connector/connector.py
@@ -705,10 +705,12 @@ class Connector:
                         else:
                             if block["height"] > self.app_block_height_on_start:
                                 block["rawTx"][q]["vIn"][i]["coin"] = await self.utxo.get_from_daemon(o)
-                                c += 1
+                                if  block["rawTx"][q]["vIn"][i]["coin"] is None:
+                                    raise Exception("utxo get failed ")
+                        c += 1
 
                 if c != ti and not self.cache_loading:
-                    self.log.critical("utxo get failed (not all utxo received)" + rh2s(block["hash"]))
+                    self.log.critical("utxo get failed (not all utxo received) " + block["hash"])
                     raise Exception("utxo get failed ")
 
             self.total_received_tx += len(block["rawTx"])
