a27ab79cf0af21b349050fa3830ccca1e46d3ee8
4tochka <admin@bitaps.com>
2019-06-18 20:09:43 +0400

    merkle tree functions

--- pybtc/connector/block_loader.py
+++ pybtc/connector/block_loader.py
@@ -112,10 +112,10 @@ class BlockLoader:
             if self.parent.block_preload._store:
                 while next(reversed(self.parent.block_preload._store)) >= target_height:
                     await asyncio.sleep(1)
-            self.log.info("block loader reached target block %s" % target_height)
-            self.log.debug("    Cache first block %s; "
-                           "cache last block %s;" % (next(iter(self.parent.block_preload._store)),
-                                                     next(reversed(self.parent.block_preload._store))))
+                self.log.info("block loader reached target block %s" % target_height)
+                self.log.debug("    Cache first block %s; "
+                               "cache last block %s;" % (next(iter(self.parent.block_preload._store)),
+                                                         next(reversed(self.parent.block_preload._store))))
 
             [self.worker[p].terminate() for p in self.worker]
             for p in self.worker_busy: self.worker_busy[p] = False
