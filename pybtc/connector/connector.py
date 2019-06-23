b7073edbdc2ae9c26e68e20ab0ff54d639edd27a
4tochka <admin@bitaps.com>
2019-06-23 19:41:47 +0400

    connector module brief description

--- pybtc/connector/connector.py
+++ pybtc/connector/connector.py
@@ -779,7 +779,7 @@ class Connector:
                                             "WHERE tx_id = ANY($1);", set(s2rh(t) for t in missed))
 
                     for row in rows:
-                        missed.remove(row["tx_id"])
+                        missed.remove(rh2s(row["tx_id"]))
                     coinbase = await conn.fetchval("SELECT out_tx_id FROM connector_unconfirmed_utxo "
                                               "WHERE out_tx_id  = $1 LIMIT 1;", s2rh(block["tx"][0]))
                     if coinbase:
