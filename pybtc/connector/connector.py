f6044a0a64426e23fbfbc8d828c8d4b373f34ffc
4tochka <admin@bitaps.com>
2019-06-27 15:48:06 +0400

    zero mq block notification

--- pybtc/connector/connector.py
+++ pybtc/connector/connector.py
@@ -918,7 +918,7 @@ class Connector:
 
     async def _new_transaction(self, tx, timestamp):
         tx_hash = rh2s(tx["txId"])
-
+        print("new", tx_hash)
         if tx_hash in self.tx_in_process or self.tx_cache.has_key(tx_hash):
             return
 
