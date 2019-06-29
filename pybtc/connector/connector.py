567311ecc10c6963a0700a387ee4ea05e695b33c
4tochka <admin@bitaps.com>
2019-06-29 18:24:46 +0400

    code clean up

--- pybtc/connector/connector.py
+++ pybtc/connector/connector.py
@@ -622,14 +622,14 @@ class Connector:
 
     async def verify_block_position(self, block):
         if "previousblockhash" not in block :
-            print(1)
+            print(11)
             return
         if self.block_headers_cache.len() == 0:
             print(2)
             return
 
         # if self.block_headers_cache.get_last_key() != block["previousblockhash"]:
-        if self.tt and 0 :
+        if self.tt :
             self.tt -= 1
             print("test orphan")
             await asyncio.sleep(30)
