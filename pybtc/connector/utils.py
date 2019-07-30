38dba97d07e3ef8dc41886d40bb8da8fa1e11c69
4tochka <admin@bitaps.com>
2019-07-30 16:13:28 +0400

    connector keep raw tx

--- pybtc/connector/utils.py
+++ pybtc/connector/utils.py
@@ -54,6 +54,7 @@ def decode_block_tx(block):
     b["tx"] = list()
     for i in range(var_int_to_int(read_var_int(s))):
         b["rawTx"][i] = Transaction(s, format="raw", keep_raw_tx=True)
+        b["rawTx"][i]["raw"]
         b["tx"].append(rh2s(b["rawTx"][i]["txId"]))
         b["amount"] += b["rawTx"][i]["amount"]
         b["strippedSize"] += b["rawTx"][i]["bSize"]
