06713c920e22c9fc3c94100630c7510291ccd50f
4tochka <admin@bitaps.com>
2019-08-19 15:43:40 +0400

    bloom filter functions

--- pybtc/functions/block.py
+++ pybtc/functions/block.py
@@ -98,10 +98,9 @@ def merkle_proof(merkle_tree, index, return_hex=True, receive_hex=False):
         return mp
 
 def merkle_root_from_proof(merkle_proof, tx_id, index, return_hex=True, receive_hex=True):
-
     if isinstance(merkle_proof, str):
-        merkle_proof = [merkle_proof[y-64:y] for y in range(64, len(merkle_proof)+64,64)]
-    elif isinstance(merkle_proof, bytes):
+        merkle_proof = bytes_from_hex(merkle_proof)
+    if isinstance(merkle_proof, bytes):
         merkle_proof = [merkle_proof[y - 32:y] for y in range(32, len(merkle_proof) + 32, 32)]
 
     if receive_hex:
