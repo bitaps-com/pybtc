8003b397474b9414d4db5a66f84c4e4fdbf4e1c5
4tochka <admin@bitaps.com>
2020-03-09 23:50:24 +0400

    tests

--- pybtc/connector/connector.py
+++ pybtc/connector/connector.py
@@ -617,6 +617,9 @@ class Connector:
 
             if self.deep_synchronization:
                 print(">>>>", self.last_block_height, self.last_block_utxo_cached_height)
+                self.log.debug("    Cache first block %s; "
+                               "cache last block %s;" % (next(iter(self.block_preload._store)),
+                                                         next(reversed(self.block_preload._store))))
                 if self.last_block_height < self.last_block_utxo_cached_height:
                     if not self.cache_loading:
                         self.log.info("Bootstrap UTXO cache ...")
