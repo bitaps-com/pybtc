bc0ec92e7fd3456e0937ea61757a8faf42ffcb05
4tochka <admin@bitaps.com>
2020-03-10 00:06:03 +0400

    tests

--- pybtc/connector/connector.py
+++ pybtc/connector/connector.py
@@ -556,9 +556,12 @@ class Connector:
 
                 if self.deep_synchronization:
                     print(">>>>", self.last_block_height + 1, self.last_block_height, self.last_block_utxo_cached_height)
-                    self.log.debug("    Cache first block %s; "
-                                   "cache last block %s;" % (next(iter(self.block_preload._store)),
-                                                             next(reversed(self.block_preload._store))))
+                    try:
+                        self.log.debug("    Cache first block %s; "
+                                       "cache last block %s;" % (next(iter(self.block_preload._store)),
+                                                                 next(reversed(self.block_preload._store))))
+                    except:
+                        pass
                     raw_block = self.block_preload.pop(self.last_block_height + 1)
                     if raw_block:
                         q = time.time()
