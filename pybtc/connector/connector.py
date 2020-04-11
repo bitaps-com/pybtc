1dbd3f96864de6232c0e37cea1bb22125c39e2fc
4tochka <admin@bitaps.com>
2020-04-11 18:24:32 +0400

    redesigned stxo txMap

--- pybtc/connector/connector.py
+++ pybtc/connector/connector.py
@@ -897,7 +897,7 @@ class Connector:
                                             va = block["_txMap"].get((r[2], tx_pointer), 0)
                                             block["_txMap"][(r[2], tx_pointer)] = va - r[1]
 
-                                            block["stxo"].append(((r[0], (height << 39) + (q << 20) + i), r[2],  r[1]))
+                                            block["stxo"].append((r[0], (height << 39) + (q << 20) + i, r[2],  r[1]))
                                     else:
                                         missed.append((inp["_outpoint"], (height<<39)+(q<<20)+i, q, i))
 
@@ -924,8 +924,8 @@ class Connector:
                             block["_txMap"][(block["rawTx"][q]["vIn"][i]["coin"][2], tx_pointer)] = \
                                 va - block["rawTx"][q]["vIn"][i]["coin"][1]
 
-                            block["stxo"].append(((block["rawTx"][q]["vIn"][i]["coin"][0],
-                                                 (height << 39)+(q<<20)+i),
+                            block["stxo"].append((block["rawTx"][q]["vIn"][i]["coin"][0],
+                                                 (height << 39)+(q<<20)+i,
                                                  block["rawTx"][q]["vIn"][i]["coin"][2],
                                                  block["rawTx"][q]["vIn"][i]["coin"][1]))
 
